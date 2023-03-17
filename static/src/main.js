101abb2d8c7baa9071259a62521e59b1f7defbb3
TimothyNjau <93652350+TimothyNjau@users.noreply.github.com>
2023-03-17 16:25:00 +0300

    Crossword_DEV
    *This is the first update of the crossword development.

---
+++ static/src/main.js
@@ -0,0 +1,8 @@
+const gridCanvas = document.querySelector(".grid"),
+      openBtn = document.querySelector("#open");
+
+let grid = [],
+    words = ['cat','mice','dog','cow','orange'];
+openBtn.addEventListener("click",()=>{
+   console.log("It works");
+});
